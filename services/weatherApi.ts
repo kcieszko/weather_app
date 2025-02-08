b7f912129cc5c0dd3477597ba5c223735f160bc0
kcieszko <cieszkokrzysztof@gmail.com>
2025-02-08 08:36:18 +0100

    added WeatherResponse type

--- services/weatherApi.ts
+++ services/weatherApi.ts
@@ -2,7 +2,63 @@ import { Coordinates } from "@/types/common";
 
 const API_KEY = "c930517040b8f3151e26a7ac9b58ea99";
 
-export const fetchWeather = async ({ lat, lon }: Coordinates) => {
+export type WeatherResponse = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level?: number;
+    grnd_level?: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust?: number;
+  };
+  rain?: {
+    "1h"?: number;
+    "3h"?: number;
+  };
+  snow?: {
+    "1h"?: number;
+    "3h"?: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type?: number;
+    id?: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+export const fetchWeather = async ({
+  lat,
+  lon,
+}: Coordinates): Promise<WeatherResponse> => {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=pl`
   );
